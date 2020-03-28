e55ab6aed9fd4264c64b32c5ebcf27de0390f312
Shounak Ghosh <shounakghosh@Shounaks-MacBook-Air.local>
2020-03-28 20:02:13 +0530

    Login and register page design and handler

---
+++ login.php
@@ -0,0 +1,102 @@
+<?php include('handlers/loginHandler.php') ?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <title>Dashboard Login</title>
+        <meta content="Dashboard" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- App Icons -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- Basic Css files -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+
+    <body class="fixed-left">
+
+        <!-- Loader -->
+        <div id="preloader"><div id="status"><div class="spinner"></div></div></div>
+
+
+        <!-- Begin page -->
+        <div class="accountbg"></div>
+        <div class="wrapper-page">
+
+            <div class="card">
+                <div class="card-body">
+
+                    <!--
+                    <h3 class="text-center m-0">
+                        <a href="login.php" class="logo logo-admin"><img src="assets/images/logo.png" height="30" alt="logo"></a>
+                    </h3>
+                    -->
+                    
+                    <div class="p-3">
+                        <h4 class="text-dark font-18 m-b-5 text-center">Welcome Back !</h4>
+                        <p class="text-muted text-center">Sign in to continue to Smarty.</p>
+
+                        <form method="post" class="form-horizontal m-t-30" action="login.php">
+                            <?php include('handlers/errorHandler.php'); ?>
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="userpassword">Password</label>
+                                <input type="password" class="form-control" id="userpassword" placeholder="Enter password" name="password">
+                            </div>
+
+                            <div class="form-group row m-t-20">
+                                <div class="col-sm-6">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                        <label class="custom-control-label" for="customControlInline">Remember me</label>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 text-right">
+                                    <button class="btn btn-primary w-md waves-effect waves-light" type="submit" name="login_user">Log In</button>
+                                </div>
+                            </div>
+
+                            <div class="form-group m-t-10 mb-0 row">
+                                <div class="col-12 m-t-20" align="center">
+                                    Don't have an account ? <a href="signup.php" class="font-500 font-14 text-white font-secondary"> Signup Now </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="m-t-40 text-center">
+                <p class="text-white">© 2020 Smarty. Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://github.com/sghosh1810" class="font-500 font-14 text-white font-secondary">Me</a></p>
+            </div>
+
+        </div>
+
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/modernizr.min.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.nicescroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.js"></script>
+
+    </body>
+</html>
