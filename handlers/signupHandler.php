1b6ef2a7ec6121793b9be304ca00447d77a53907
Shounak Ghosh <shounakghosh@Shounaks-MacBook-Air.local>
2020-03-30 15:35:50 +0530

    Added userDetaailsHandler and callbacks

--- handlers/signupHandler.php
+++ handlers/signupHandler.php
@@ -7,8 +7,8 @@
     $errors = array(); 
 
     // connect to the database
-    $db = mysqli_connect('localhost', 'root', '', 'refrigerg');
-    //$db = mysqli_connect('eu-cdbr-west-02.cleardb.net','b24bdb07f8df3b','98354db2','heroku_d0b67b039063502');
+    //$db = mysqli_connect('localhost', 'root', '', 'refrigerg');
+    $db = mysqli_connect('eu-cdbr-west-02.cleardb.net','b24bdb07f8df3b','98354db2','heroku_d0b67b039063502');
 
 
     // REGISTER USER
