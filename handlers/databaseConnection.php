09baf6e39cdcc671729374116b111715f0f47133
Shounak Ghosh <shounakghosh@Shounaks-MacBook-Air.local>
2020-03-31 00:57:14 +0530

    Fixed for heroku deploy

---
+++ handlers/databaseConnection.php
@@ -0,0 +1,4 @@
+<?php
+    $db = mysqli_connect('eu-cdbr-west-02.cleardb.net','b24bdb07f8df3b','98354db2','heroku_d0b67b039063502');
+    //$db = mysqli_connect('localhost', 'root', '', 'refrigerg');
+?>
