906b0bf78dff0c28f864ee4a5e7c56e7a17a10ed
Shounak Ghosh <shounakghosh@Shounaks-MacBook-Air.local>
2020-04-06 18:15:15 +0530

    Fixed Reciepe Recommendor page header

--- app/user/recieperecommender.php
+++ app/user/recieperecommender.php
@@ -25,7 +25,7 @@ if (isset($_GET['logout'])) {
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <title>Inventory List</title>
+        <title>Reciepe Recommender</title>
         <meta content="Admin Dashboard" name="description" />
         <meta content="Themesbrand" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
